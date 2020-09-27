4aff8e49d1941c6b4f4f32e747e8a80fbe2bc4fe
xjyssg <jiayuexue30@gmail.com>
2020-09-27 09:32:31 +0800

    5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'root',
+      username: 'root',
+      password: 'root'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,24 @@ describe('Blog app', function() {
     cy.contains('log in to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('root')
+      cy.get('#login-button').click()
+
+      cy.wait(10000)
+      cy.contains('root logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('123')
+      cy.get('#login-button').click()
+
+      cy.contains('Wrong credentials')
+    })
   })
 })
